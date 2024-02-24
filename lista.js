81da7248a78a89821be97adb7e159b26ffed146a
Isti-k <kiscserkes03@gmail.com>
2024-02-24 20:41:29 +0100

    hf

---
+++ lista.js
@@ -0,0 +1,29 @@
+const autoklista=[
+    {
+        marka:"audi",
+        tipus:"r8",
+        szin:"ezüst",
+        gyartasi_ev:"2023"
+    },
+    {
+        marka:"mercedés",
+        tipus:"amg",
+        szin:"fekete",
+        gyartasi_ev:"2022"
+    },
+    {
+        marka:"bmw",
+        tipus:"x5",
+        szin:"fekete",
+        gyartasi_ev:"2022"
+    }
+]
+
+//Hogyan érjük el az audi nevét?
+console.log(autoklista[0].marka)
+
+//Hogyan érjük ek a mercédes színét?
+console.log(autoklista[1].szin)
+
+//Hogyan érjük ek a bmw típusát?
+console.log(autoklista[2].tipus)
